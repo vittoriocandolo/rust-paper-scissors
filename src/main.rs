f195c198f18df1551fb8aec13bc43481844658a6
vittoriocandolo <56765332+vittoriocandolo@users.noreply.github.com>
2022-12-31 12:22:53 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::thread;
+use std::sync::{Arc, Mutex};
+
+fn main(){
+    let n_th = 2;
+    let hands = Arc::new(Mutex::new(vec![0; n_th]));
+    let mut threads = Vec::new();
+    for i in 0..n_th {
+        let hands = hands.clone();
+        threads.push(thread::spawn(move || {
+            let mut hands = hands.lock().unwrap();
+            let hand = rand::random::<u8>() % 3;
+            hands[i] = hand;
+        }));
+    }
+    for thread in threads {
+        thread.join().unwrap();
+    }
+    let hands = hands.lock().unwrap();
+    let hand1 = hands[0];
+    let hand2 = hands[1];
+    if hand1 == hand2 {
+        println!("Tie!");
+    } else if (hand1 == 0 && hand2 == 2) || (hand1 == 1 && hand2 == 0) || (hand1 == 2 && hand2 == 1){
+        println!("Player 1 wins!");
+    } else {
+        println!("Player 2 wins!");
+    }
+}
